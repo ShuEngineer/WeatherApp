396712474ddc8a15d5db6aec38bf662dd93d6245
ShuEngineer <supercar110shu@gmail.com>
2020-02-02 00:32:35 +0900

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by 伊藤嵩 on 2020/02/02.
+//  Copyright © 2020 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
